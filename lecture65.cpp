01f0f2a51e6393eed64af8583101b9f00030eb58
Suraj Pratap <92919173+SurajPratap10@users.noreply.github.com>
2023-09-21 08:00:07 +0530

    Update lecture65.cpp

--- lecture65.cpp
+++ lecture65.cpp
@@ -1,92 +1,62 @@
-// #include <iostream>
+// #include<iostream>
+// #include<queue>
 // using namespace std;
 
-// Q) N stacks in an array:
+// int main() {
+//     cout << (-1)%1<< endl;
+//     deque<int> d;
 
-// class NStack
-// {
+//     d.push_front(12);
+//     d.push_back(14);
 
-//     int *arr;
-//     int *top;
-//     int *next;
+//     cout << d.front() << endl;
+//     cout << d.back() << endl;
 
-//     int n, s;
-//     int freespot;
+//     d.pop_front();
 
-// public:
-//     // Initialize your data structure.
-//     NStack(int N, int S)
-//     {
-//         n = N;
-//         s = S;
-//         arr = new int[s];
-//         top = new int[n];
-//         next = new int[s];
+//     cout << d.front() << endl;
+//     cout << d.back() << endl;
+//     d.pop_back();
 
-//         //top initialise
-//         for(int i=0; i<n; i++) {
-//             top[i] = -1;
-//         }
-
-//         //next initialise
-//         for(int i=0; i<s; i++) {
-//             next[i] = i+1;
-//         }
-//         //update last index value to -1
-//         next[s-1] = -1;
-
-//         //initialise freespot
-//         freespot = 0;
-
+//     if(d.empty()) {
+//         cout << "queue is empty" << endl;
+//     }
+//     else{
+//         cout << "queue is not empty " << endl;
 //     }
 
-//     // Pushes 'X' into the Mth stack. Returns true if it gets pushed into the
-//     stack, and false otherwise. bool push(int x, int m)
-//     {
-//         //check for overflow
-//         if(freespot == -1) {
-//             return false;
-//         }
+//     /*
+//     //create a queue
+//     queue<int> q;
 
-//         //find index
-//         int index = freespot;
+//     q.push(11);
+//     cout << "front of q is: " << q.front() << endl;
+//     q.push(15);
+//     cout << "front of q is: " << q.front() << endl;
 
-//         //insert element into array
-//         arr[index] = x;
+//     q.push(13);
+//     cout << "front of q is: " << q.front() << endl;
 
-//         //update freespot
-//         freespot = next[index];
+//     cout << "size of queue is : " << q.size() << endl;
 
-//         //update next;
-//         next[index] = top[m-1];
+//     q.pop();
+//     q.pop();
+//     q.pop();
 
-//         //update top
-//         top[m-1] = index;
+//     cout << "size of queue is : " << q.size() << endl;
 
-//         return true;
+//     if(q.empty()) {
+//         cout << "Queue is empty " << endl;
 //     }
-
-//     // Pops top element from Mth Stack. Returns -1 if the stack is empty,
-//     otherwise returns the popped element. int pop(int m)
-//     {
-//         //check underflow condition
-//         if(top[m-1] == -1) {
-//             return -1;
-//         }
-
-//         int index= top[m-1];
-
-//         top[m-1] = next[index];
-
-//         next[index] = freespot;
-
-//         freespot = index;
-
-//         return arr[index];
+//     else{
+//         cout << "queue is not empty " << endl;
 //     }
-// };
+//     */
 
-// Q1) Implement a QUEUE:
+//     return 0;
+// }
+
+// Q1) Implement a Queue:
 
 // class Queue {
 
@@ -104,8 +74,7 @@
 //     }
 
 //     /*----------------- Public Functions of Queue -----------------*/
-
-//     bool isEmpty() {
+//  bool isEmpty() {
 //         if(qfront == rear) {
 //             return true;
 //         }
@@ -148,6 +117,257 @@
 //         else
 //         {
 //             return arr[qfront];
+//         }
+//     }
+//  };
+
+// Q2) Circular Queue:
+
+// class MyCircularQueue {
+//     int *arr;
+//     int front;
+//     int rear;
+//     int size;
+// public:
+//     MyCircularQueue(int k) {
+//         this->size = k;
+//         arr = new int[size];
+//         front = -1;
+//         rear = -1;
+//     }
+
+//     bool enQueue(int value) {
+//         //Queue Full
+//         if( isFull()){
+//             return false;
+//         }
+
+//         //First element push
+//         else if(rear==-1 && front==-1){
+//             front=0;
+//             rear=0;
+//             arr[rear]=value;
+//         }
+
+//         //Rear at last index (cyclic nature)
+//         else if(front!=0 && rear==size-1){
+//             rear=0;
+//             arr[rear]=value;
+//         }
+
+//         //normal condition
+//         else{
+//             rear++;
+//             arr[rear]=value;
+//         }
+//         return true;
+//     }
+
+//     bool deQueue() {
+//         //Empty Queue or not
+//         if(isEmpty()){
+//             return false;
+//         }
+
+//         int ans=arr[front];
+//         arr[front]=-1;
+
+//         //Single element check
+//         if(front==rear){
+//             front=-1;
+//             rear=-1;
+//         }
+
+//         //front at last index (cyclic nature)
+//         else if(front==size-1){
+//             front=0;
+//         }
+
+//         else{
+//             front++;
+//         }
+
+//         return true;
+//     }
+
+//     int Front() {
+//          if(isEmpty()) {
+//             return -1;
+//         }
+//         return arr[front];
+//     }
+
+//     int Rear() {
+//         if(isEmpty()) {
+//             return -1;
+//         }
+//         return arr[rear];
+//     }
+
+//     bool isEmpty() {
+//         if(front==-1 ){
+//             return true;
+//         }
+//         else{
+//             return false;
+//         }
+//     }
+
+//     bool isFull() {
+//         if( (front == 0 && rear == size-1)||(rear == (front-1)%(size)) ){
+//             return true;
+//         }
+
+//         else{
+//             return false;
+//         }
+//     }
+// };
+
+// Q3) Doubly Ended Queue (Dequeue):
+
+// class Deque
+// {
+//     int *arr;
+//     int front;
+//     int rear;
+//     int size;
+// public:
+//     // Initialize your data structure.
+//     Deque(int n)
+//     {
+//         size = n;
+//         arr = new int[n];
+//         front = -1;
+//         rear = -1;
+//     }
+
+//     // Pushes 'X' in the front of the deque. Returns true if it gets pushed
+//     into the deque, and false otherwise. bool pushFront(int x)
+//     {
+//         //check full or not
+//         if( isFull() ) {
+//             return false;
+//         }
+//         else if(isEmpty()) {
+//             front  = rear = 0;
+//         }
+//         else if(front == 0 && rear != size-1) {
+//             front = size-1;
+//         }
+//         else
+//         {
+//             front--;
+//         }
+//         arr[front] = x;
+//         return true;
+//     }
+
+//     // Pushes 'X' in the back of the deque. Returns true if it gets pushed
+//     into the deque, and false otherwise. bool pushRear(int x)
+//     {
+//         if( isFull() ) {
+//             return false;
+//         }
+//         else if(isEmpty()) {
+//             front  = rear = 0;
+//         }
+//         else if(rear == size-1 && front != 0) {
+//             rear = 0;
+//         }
+//         else
+//         {
+//              rear++;
+//         }
+//         arr[rear] = x;
+//         return true;
+//     }
+
+//     // Pops an element from the front of the deque. Returns -1 if the deque
+//     is empty, otherwise returns the popped element. int popFront()
+//     {
+//         if(isEmpty()){//to check queue is empty
+//             //cout << "Queue is Empty " << endl;
+//             return -1;
+//         }
+
+//         int ans = arr[front];
+//         arr[front] = -1;
+
+//         if(front == rear) { //single element is present
+//             front = rear = -1;
+//         }
+//         else if(front == size - 1) {
+//             front = 0; //to maintain cyclic nature
+//         }
+//         else
+//         {//normal flow
+//             front++;
+//         }
+//         return ans;
+//     }
+
+//     // Pops an element from the back of the deque. Returns -1 if the deque is
+//     empty, otherwise returns the popped element. int popRear()
+//     {
+//          if(isEmpty()){//to check queue is empty
+//             //cout << "Queue is Empty " << endl;
+//             return -1;
+//         }
+
+//         int ans = arr[rear];
+//         arr[rear] = -1;
+
+//         if(front == rear) { //single element is present
+//             front = rear = -1;
+//         }
+//         else if(rear == 0) {
+//             rear = size-1; //to maintain cyclic nature
+//         }
+//         else
+//         {//normal flow
+//             rear--;
+//         }
+//         return ans;
+//     }
+
+//     // Returns the first element of the deque. If the deque is empty, it
+//     returns -1. int getFront()
+//     {
+//         if(isEmpty()){
+//             return -1;
+//         }
+//         return arr[front];
+//     }
+
+//     // Returns the last element of the deque. If the deque is empty, it
+//     returns -1. int getRear()
+//     {
+//        if(isEmpty()){
+//             return -1;
+//         }
+//         return arr[rear];
+//     }
+
+//     // Returns true if the deque is empty. Otherwise returns false.
+//     bool isEmpty()
+//     {
+//         if(front == -1)
+//             return true;
+//         else
+//             return false;
+//     }
+
+//     // Returns true if the deque is full. Otherwise returns false.
+//     bool isFull()
+//     {
+// 		if( (front == 0 && rear == size-1) || (front != 0 && rear ==
+// (front-1)%(size-1) ) ) {
+//             return true;
+//         }
+//         else
+//         {
+//             return false;
 //         }
 //     }
 // };
