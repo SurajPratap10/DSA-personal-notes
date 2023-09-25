37d5f384954f9256396c891b12d58426bd3be05a
Suraj Pratap <92919173+SurajPratap10@users.noreply.github.com>
2023-09-25 23:54:38 +0530

    Update lecture69.cpp

--- lecture69.cpp
+++ lecture69.cpp
@@ -103,4 +103,75 @@ using namespace std;
 //     }
 // };
 
+
 // Q5) Identical Trees:
+
+// bool isSameTree(TreeNode* p, TreeNode* q) {
+//         //Base Cases:
+//         if(p == NULL && q == NULL){
+//             return true;
+//         }
+//         if(p == NULL && q != NULL){
+//             return false;
+//         }
+//         if(p != NULL && q == NULL){
+//             return false;
+//         }
+//         else{
+//             bool left = isSameTree(p->left, q->left);
+//             bool right = isSameTree(p->right, q->right);
+
+//             bool value = p->val == q->val;
+
+//             if(left && right && value){
+//                 return true;
+//             }
+//             else{
+//                 return false;
+//             }
+//         }
+//     }
+
+// Q7) Sum Tree:
+
+// pair<bool,int> isSumTreeFast(Node* root) {
+
+//         //base case
+//         if(root == NULL) {
+//             pair<bool,int> p = make_pair(true, 0);
+//             return p;
+//         }
+
+//         if(root->left == NULL && root->right == NULL ) {
+//             pair<bool,int> p = make_pair(true, root->data);
+//             return p;
+//         }
+
+//         pair<bool,int> leftAns = isSumTreeFast(root->left);
+//         pair<bool,int> rightAns = isSumTreeFast(root->right);
+
+//         bool isLeftSumTree = leftAns.first;
+//         bool isRightSumTree = rightAns.first;
+
+//         int leftSum = leftAns.second;
+//         int rightSum = rightAns.second;
+
+//         bool condn = root->data == leftSum + rightSum;
+
+//         pair<bool, int> ans;
+
+//         if(isLeftSumTree && isRightSumTree && condn) {
+//             ans.first = true;
+//             ans.second = root->data + leftSum + rightSum ;
+//         }
+//         else
+//         {
+//             ans.first = false;
+//         }
+//         return ans;
+
+//     }
+//     bool isSumTree(Node* root)
+//     {
+//         return isSumTreeFast(root).first;
+//     }
