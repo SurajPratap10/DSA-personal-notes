78dc480b17dd6cd487eee3d811d1359cb4d233e2
Suraj Pratap <92919173+SurajPratap10@users.noreply.github.com>
2023-10-14 11:55:35 +0530

    Create lecture87.cpp

---
+++ lecture87.cpp
@@ -0,0 +1,77 @@
+// #include <iostream>
+// using namespace std;
+
+// TRIES:
+
+// Q1) Implement a Trie(Prefix Tree):
+
+// class TrieNode {
+// public:
+//   char data;
+//   TrieNode *children[26];
+//   bool isTerminal;
+//   TrieNode(char ch) {
+//     data = ch;
+//     for (int i = 0; i < 26; i++)
+//       children[i] = NULL;
+//     isTerminal = false;
+//   }
+// };
+// class Trie {
+
+// public:
+//   /** Initialize your data structure here. */
+//   TrieNode *root;
+//   Trie() { root = new TrieNode('\0'); }
+
+//   /** Inserts a word into the trie. */
+//   void insertUtil(TrieNode *root, string word) {
+//     if (word.length() == 0) {
+//       root->isTerminal = true;
+//       return;
+//     }
+//     int index = word[0] - 'a';
+//     TrieNode *child;
+//     if (root->children[index] != NULL) {
+//       child = root->children[index];
+//     } else {
+//       child = new TrieNode(word[0]);
+//       root->children[index] = child;
+//     }
+//     insertUtil(child, word.substr(1));
+//   }
+//   void insert(string word) { insertUtil(root, word); }
+
+//   /** Returns if the word is in the trie. */
+//   bool searchUtil(TrieNode *root, string word) {
+//     if (word.length() == 0) {
+//       return root->isTerminal;
+//     }
+//     int index = word[0] - 'a';
+//     TrieNode *child;
+//     if (root->children[index] != NULL) {
+//       child = root->children[index];
+//     } else {
+//       return false;
+//     }
+//     return searchUtil(child, word.substr(1));
+//   }
+//   bool search(string word) { return searchUtil(root, word); }
+
+//   /** Returns if there is any word in the trie that starts with the given
+//    * prefix. */
+//   bool prefixUtil(TrieNode *root, string prefix) {
+//     if (prefix.length() == 0) {
+//       return true;
+//     }
+//     int index = prefix[0] - 'a';
+//     TrieNode *child;
+//     if (root->children[index] != NULL) {
+//       child = root->children[index];
+//     } else {
+//       return false;
+//     }
+//     return prefixUtil(child, prefix.substr(1));
+//   }
+//   bool startsWith(string prefix) { return prefixUtil(root, prefix); }
+// };
