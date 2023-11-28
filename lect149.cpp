a667c76f80cba4dc7ae46dfaf41eaef1012f07ca
Suraj Pratap <92919173+SurajPratap10@users.noreply.github.com>
2023-11-28 21:07:39 +0530

    Create lect149.cpp

---
+++ lect149.cpp
@@ -0,0 +1,34 @@
+// #include <iostream>
+// using namespace std;
+
+// DYNAMIC PROGRAMMING (DP):
+
+// GREEDY ALGORTITHMS:
+
+// Q1) N-meetings in One-Room:
+
+// bool cmp (pair<int, int> a, pair<int, int> b){
+//   return a.second < b.second;
+// }
+
+// int maxMeetings(int start[] int end[], int n){
+//   vector<pair<int int>> v;
+//   for(int i = 0; i <= n; i++){
+//     pair<int, int> p = make_pair(start[i], end[i]);
+//     v.push_back(p);
+//   }
+
+//   sort(v.begin(), v.end());
+//   int cnt = 1;
+//   int ansEnd = v[0].second;
+
+//   for(int i = 1; i < n; i++){
+//     if(v[i].first > ansEnd){
+//       cnt++;
+//       ansEnd = v[i].second;
+//     }
+//   }
+//   return cnt;
+// }
+
+// Q2) Shop in a candy store:
