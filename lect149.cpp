e3601f3cdc93aa384a3d26e4ebc09ffe8c5fff2a
Suraj Pratap <92919173+SurajPratap10@users.noreply.github.com>
2023-12-03 09:34:00 +0530

    Update lect149.cpp

--- lect149.cpp
+++ lect149.cpp
@@ -123,3 +123,30 @@
 //   }
 //   return mini;
 // }
+
+// Q6) Minimum Cost Of N Ropes:
+
+// long long minCost(long long arr[], long long n) {
+//   priority_queue<long long, vector<long long>, greater<long long>> pq;
+
+//   for (int i = 0; i < n; i++) {
+//     pq.push(arr[i]);
+//   }
+
+//   long long cost = 0;
+//   while (pq.size() > 1) {
+//     long long first = pq.top();
+//     pq.pop();
+//     long long second = pq.top();
+//     pq.pop();
+
+//     long long mwrgedLength = first + second;
+//     cost = cost + mergedLength;
+
+//     pq.push(mergedLength);
+//   }
+//   return cost;
+//   }
+// }
+
+// Q7) Huffman Encoding:
